2ede9830f3bdf3816d64a2b14a3671047e533389
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-11-03 15:07:21 +0300

    fix: Fixed prisma setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  const port = process.env.PORT ?? 5001;
+  await app.listen(port);
+  console.log('Backend is running on port: ', port);
 }
 bootstrap();
