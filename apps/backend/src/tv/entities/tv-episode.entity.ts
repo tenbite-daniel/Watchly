254a4753a6b2fde9c8d1c76c06fe55f8365b9083
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-11-09 00:44:55 +0300

    feat(graphql): add TvEpisode entity with all its relationships

---
+++ apps/backend/src/tv/entities/tv-episode.entity.ts
@@ -0,0 +1,52 @@
+import { ObjectType, Field, Int, ID, Float } from '@nestjs/graphql';
+import { GraphQLJSON } from 'graphql-type-json';
+import { TvSeason } from './tv-season.entity';
+import { EpisodeProgress } from './episode-progress.entity';
+
+@ObjectType()
+export class TvEpisode {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  season_id: string;
+
+  @Field(() => Int)
+  episode_number: number;
+
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field({ nullable: true })
+  overview?: string;
+
+  @Field({ nullable: true })
+  air_date?: Date;
+
+  @Field(() => Int, { nullable: true })
+  runtime?: number;
+
+  @Field({ nullable: true })
+  still_path?: string;
+
+  @Field(() => Float, { nullable: true })
+  vote_average?: number;
+
+  @Field(() => Int, { nullable: true })
+  vote_count?: number;
+
+  @Field(() => GraphQLJSON, { nullable: true })
+  episode_data: any;
+
+  @Field()
+  updated_at: Date;
+
+  @Field()
+  created_at: Date;
+
+  @Field(() => TvSeason)
+  tv_season: TvSeason;
+
+  @Field(() => [EpisodeProgress], { nullable: true })
+  episode_progress?: EpisodeProgress[];
+}
