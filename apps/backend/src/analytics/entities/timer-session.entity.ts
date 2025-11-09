dc99793dde0edee5de01941e82a4d4a6dfa1f044
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-11-09 17:59:33 +0300

    feat(graphql): add TimerSession entity for timer state

---
+++ apps/backend/src/analytics/entities/timer-session.entity.ts
@@ -0,0 +1,39 @@
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { TimerHistory } from './timer-history.entity';
+import { User } from 'src/user/entities/user.entity';
+
+@ObjectType()
+export class TimerSession {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  user_id: string;
+
+  @Field(() => Int)
+  total_seconds: number;
+
+  @Field(() => Int)
+  remaining_seconds: number;
+
+  @Field(() => Boolean)
+  is_paused: boolean;
+
+  @Field()
+  last_started_at: Date;
+
+  @Field()
+  daily_reset_time: string;
+
+  @Field()
+  updated_at: Date;
+
+  @Field()
+  created_at: Date;
+
+  @Field(() => User)
+  user: User;
+
+  @Field(() => [TimerHistory])
+  session: TimerHistory[];
+}
